be8b01d9d3e8b0f4a460d690f378028173628224
xuxiangqian <xuxiangqian@360.cn>
2019-03-01 17:48:43 +0800

    first commit

---
+++ string-functions.go
@@ -0,0 +1,26 @@
+package main
+
+import s "strings"
+import "fmt"
+
+var p = fmt.Println
+
+func main() {
+
+	var err error
+
+	_, err = p("Contains", s.Contains("test", "es"))
+	_, err = p("Count", s.Count("test", "s"))
+	_, err = p("HasPrefix", s.HasPrefix("test", "te"))
+	_, err = p("HasSuffix", s.HasSuffix("test", "st"))
+	_, err = p("Index", s.Index("test", "s"))
+	_, err = p("Join", s.Join([]string{"aa", "bb", "cc"}, "-"))
+	_, err = p("Repeat", s.Repeat("aa", 3))
+	_, err = p("Replace", s.Replace("test", "es", "ex", 1))
+	_, err = p("Split", s.Split("aa-bb-cc-dd", "-"))
+	_, err = p("ToLower", s.ToLower("Test"))
+	_, err = p("ToUpper", s.ToUpper("teSt"))
+	if err != nil {
+		panic(err)
+	}
+}
